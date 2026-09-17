b0e2f489c3d46cfbcdb1ec407313136f6cce45c9
lmeller-git <louis.meller@icloud.com>
2026-09-17 16:18:23 +0200

    update top

--- kasino-topology/src/lib.rs
+++ kasino-topology/src/lib.rs
@@ -21,8 +21,6 @@ use kasino::{
     strategy::{Hooked, Strategy, padded::NoPadding},
 };
 
-use crate::sync::atomic::{AtomicUsize, Ordering};
-
 /// todo
 #[derive(Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Hash)]
 pub struct CoreID(pub usize);
@@ -45,7 +43,6 @@ pub trait RuntimeData {
 /// todo
 #[derive(Debug, Default)]
 pub struct CorePinned<R> {
-    counter: AtomicUsize,
     _dat: PhantomData<R>,
 }
 
@@ -69,13 +66,8 @@ impl<R> Clone for CorePinnedGambler<R> {
 impl<R: RuntimeData> CorePinnedGambler<R> {
     /// todo
     #[inline]
-    pub fn pin_thread(&mut self) -> Result<(), R::Error> {
-        if let Ok(aff) = R::get_affinity() {
-            self.affinity = aff;
-            Ok(())
-        } else {
-            R::set_affinity(self.affinity)
-        }
+    pub fn pin_thread(&mut self, core: CoreID) {
+        self.affinity = core;
     }
 }
 
@@ -105,24 +97,49 @@ impl<Q: Collection, R: RuntimeData> Strategy<Q> for CorePinned<R> {
         *parent
     }
 
-    #[expect(clippy::missing_inline_in_public_items)]
+    #[inline]
     fn create_gambler(&self) -> Self::Gambler {
-        let affinity = match R::get_affinity() {
-            Ok(aff) => aff,
-            Err(_) => {
-                let total_cores = R::available_cores().count();
-                let count = self.counter.fetch_add(1, Ordering::Relaxed);
-                let idx = count % total_cores;
-                let aff = R::available_cores().nth(idx).unwrap();
-                debug_assert!(R::set_affinity(aff).is_ok());
-                aff
-            }
-        };
-
         CorePinnedGambler {
-            affinity,
+            affinity: Default::default(),
             _dat: PhantomData,
         }
+    }
+
+    #[inline]
+    fn on_poll_fail<'b, 'c>(
+        &self,
+        _state: &impl kasino::storage::StorageBackend<<Self::Gambler as Hooked>::Stake>,
+        _bandit_arms: &'c impl kasino::storage::StorageBackend<Q>,
+        _input: <<Q as Collection>::PollSignature as kasino::prelude::Signature>::Input<'b>,
+        _gambler: &mut Self::Gambler,
+    ) -> Option<(
+        <<Q as Collection>::PollSignature as kasino::prelude::Signature>::Output<'b, 'c>,
+        usize,
+    )>
+    where
+        Q: 'c,
+    {
+        None
+    }
+
+    #[inline]
+    fn on_offer_fail<'b, 'c>(
+        &self,
+        _state: &impl kasino::storage::StorageBackend<<Self::Gambler as Hooked>::Stake>,
+        _bandit_arms: &'c impl kasino::storage::StorageBackend<Q>,
+        input: <<Q as Collection>::OfferSignature as kasino::prelude::Signature>::Error<'b, 'c>,
+        _gambler: &mut Self::Gambler,
+    ) -> Result<
+        (
+            <<Q as Collection>::OfferSignature as kasino::prelude::Signature>::Output<'b, 'c>,
+            usize,
+        ),
+        <<Q as Collection>::OfferSignature as kasino::prelude::Signature>::Error<'b, 'c>,
+    >
+    where
+        Q: 'c,
+    {
+        Err(input)
     }
 }
 
